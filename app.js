cded3ce3ffd52b33050cf913e1fbf6bdbc656c92
phrinne <75909538+phrinne@users.noreply.github.com>
2021-06-29 11:06:55 +0300

    update app again #skip

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 app.get('/version', (req, res) => {
-  res.send('0.0.4 (not yet)')
+  res.send('0.0.4 (not)')
 })
 
 app.listen(PORT, () => {
